c05f3922e68efd48d72953e1e4c4c28692510539
AeroFortune <extraniastravesiasdejojo@gmail.com>
2022-11-22 15:52:14 -0500

    ey mueve

---
+++ index.js
@@ -0,0 +1,30 @@
+let panes = [];
+
+function ObtenerDatos(){
+    fetch('').
+    then(resultado => {
+        resultado.json().then(json => {
+            panes = json.results;
+            MapearPan();
+        })
+    });
+}
+
+function MapearPan(){
+
+}
+
+function MapearPlantilla(){
+
+}
+
+
+{/* <article class="articulo">
+                <img src="/img/pan-maiz.jpg" alt="">
+                <h1>Pan de maíz</h1>
+                <br>
+                <p>Se elabora con harina de maíz que no contiene gluten, lo que hace que sea un pan apto para celíacos.
+                </p>
+                <br>
+                <button class="button">Learn More</button>
+            </article> */}
