fabe49cd732b5c9c8170912a5d1a168a5584d01c
Diego <extraniastravesiasdejojo@gmail.com>
2022-11-23 20:55:26 -0500

    cambios + integracion panaderia

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
+let baseUrl = "http://localhost:8080";
 let panes = [];
 
-function ObtenerDatos(){
-    fetch('').
+function ObtenerPanes(){
+    fetch(baseUrl + '/panaderia/all').
     then(resultado => {
         resultado.json().then(json => {
-            panes = json.results;
-            MapearPan();
+            panes = json;
+            console.log(panes);
+            ImprimirPanes();
         })
     });
 }
 
-function MapearPan(){
-    let contenedor = document.getElementById('id');
-    for(let i=0;i<panes.length;i++){
-        contenedor.innerHTML+=MapearPlantilla(panes[]);
-    }
+function ImprimirPanes(){
+    let contenedor = document.getElementById("seccionArticulos");
 
+    panes.forEach(panes => {
+        contenedor.innerHTML += MapearPlantilla(panes);
+    });
 }
 
+function MapearPlantilla(panes){
+    return  `<article class="articulo">
+    <img src="${panes.fotoUrl}" alt="">
+    <h1>${panes.nombre}</h1>
+    <br>
+    <p>${panes.descripcion}
+    </p>
+    <br>
+    <button class="button">Aprende más</button>
+    </article>`;
 }
-
-function MapearPlantilla(){
-
-}
-
-
-{/* <article class="articulo">
-                <img src="/img/pan-maiz.jpg" alt="">
-                <h1>Pan de maíz</h1>
-                <br>
-                <p>Se elabora con harina de maíz que no contiene gluten, lo que hace que sea un pan apto para celíacos.
-                </p>
-                <br>
-                <button class="button">Learn More</button>
-            </article> */}
